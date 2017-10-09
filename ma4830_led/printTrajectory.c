8197f9be73363866756e401875ee6d562d6bcdcf
youliang <max@nutonomy.com>
2017-10-10 01:27:49 +0800

    scaling and comment of printTrajectory.c

--- ma4830_led/printTrajectory.c
+++ ma4830_led/printTrajectory.c
@@ -4,91 +4,85 @@
 #include <string.h>
 #include "printTrajectory.h"
 
+// change ScreenDisplayScale between 0 to 1 according to the resolution of the computer display 
+int Xscale, Yscale, col;
+float ScreenDisplayScale = 0.8;
+
+
+//auto scalling of x y axis according to max display
+//scale length of x and y axis is equal, helps in visulizing projectile motion
 DuoValues XYscalling(float yMax, float dMax){
 	DuoValues vals;
-	vals.deltaY = yMax/50;
-	vals.deltaX = dMax/150;
+	vals.deltaY = yMax/Xscale;
+	vals.deltaX = dMax/Yscale;
 
 	if (vals.deltaY < 2*vals.deltaX)
 		vals.deltaY = 2*vals.deltaX;
-
 	else
 		vals.deltaX = vals.deltaY/2;
-	
 	return vals;
 }
 
+//trajectory xy position while object moving upwards (vy >0)
 float Downfunc(float y){
-
 	float sqrtEq = 1 +  ((2*G_ACC*(h -y))/((pow(sin(theta),2)*pow(v,2))));
 	float d = (pow(v,2)/(2*G_ACC))  *  (1 +  sqrt(sqrtEq) )  *  sin(2*theta);
 	return d;
 
 }
 
+//trajectory xy position while object droping downwards (vy <0)
 float Upfunc(float y){
-
 	float sqrtEq = 1 +  ((2*G_ACC*(h - y))/((pow(sin(theta),2)*pow(v,2))));
 	float d = (pow(v,2)/(2*G_ACC))  *  (1 -  sqrt(sqrtEq) )  *  sin(2*theta);
 	return d;
 
 }
 
-//print spaces
+//print empty spaces 
 int printSpaces(int num){
-
+	
 	char spaces[200];
-	// char * spaces;    
-	//   // allocate memory for the new string.
-	//   spaces = malloc(num+1);
-	//   // check that the allocation was successful
-	//   if (spaces==NULL)
-	//     return NULL;
 	int i;
 	for (i=0; i<num; i++)
 		spaces[i] = ' ';
 
 	spaces[num] = '\0';
-
 	printf("%s",spaces);
-	// printf("x");
 	
 	return 0;
-
 }
 
+//plot the graph which y is above h, initial height of the object. plot from  y=h to y=yMax
 float PlotAboveH(float y, float yMax, float deltaX, float deltaY, int Yindent){
 
-	char str[200], yStr[20];
+	char str[col], yStr[20];
 	int idx;
 
 	while(y >= h && y<= yMax){
-		int upX = ceil(Upfunc(y-0.0001)/deltaX);
-		int downX = ceil(Downfunc(y-0.0001)/deltaX);
+		int upX = ceil(Upfunc(y-0.001)/deltaX);
+		int downX = ceil(Downfunc(y-0.001)/deltaX);
 
-		// printf("%d,%d\n",upX, downX);
-		//front unit 
+		//print front unit of y scale
 		sprintf(yStr, "%d", (int)y);
 		printf("%.1f",y);
 		printSpaces(Yindent - (unsigned)strlen(yStr));
-		// printf("|");
-		for (idx = 0; idx<180; idx++){
 
+		//plot xy position of the point on graph
+		for (idx = 0; idx<col; idx++){
 			if (idx == upX || idx == downX)
 				str[idx] = 'x';
 			else
 				str[idx] = ' ';
-
 		}
-		str[180] = '\0';    
+		str[col] = '\0';    
 		printf("|%s\n",str);
 		y = y - deltaY;
 	}
-
 	return y;
-
 }
 
+//plot the graph which y is below h, initial height of the object. plot from  y=h to y=0
 float PlotBelowH(float y, float deltaX, float deltaY, int Yindent){
 
 	char str[200], yStr[20];
@@ -97,64 +91,60 @@ float PlotBelowH(float y, float deltaX, float deltaY, int Yindent){
 
 		int downX = ceil(Downfunc(y)/deltaX);
 
-		// printf("%d,%d\n",upX, downX);
-		//front unit 
+		//print front unit of y scale
 		sprintf(yStr, "%d", (int)y);
 		printf("%.1f",y);
 		printSpaces(Yindent - (unsigned)strlen(yStr));
-		// printf("|");
 
-		for (idx = 0; idx<180; idx++){
-
+		//plot xy position of the point on graph
+		for (idx = 0; idx<col; idx++){
 			if (idx ==  downX)
 				str[idx] = 'x';
 			else
 				str[idx] = ' ';
-
 		}
 
-		str[180] = '\0';    
+		str[col] = '\0';    
 		printf("|%s\n",str);
-		// printf("this indent %d, %d ", Yindent, (unsigned)strlen(yStr));
 		y = y - deltaY;
-
 	}
 	return y;
-
 }
 
+//plot all scale on the x axis, consist of 2 lines of printing
 int PlotXaxis(int Yindent, float deltaX, float dMax){
-	//print "_"
+	
 	char str[200], xDigit[20];
 	int i, idx=1, interval = 10;
 
-	for (i=0; i<180; i++)
+	//print "_" seperation line
+	for (i=0; i<col; i++)
 		str[i] = '_';
-
-	str[180] = '\0';
+	str[col] = '\0';
 	printf("%s\n",str);
 
-	// strcpy( str, "(0,0)");
+	//initial indentation
 	printf("(0,0)");
 	printSpaces(Yindent-2);
 
-	while(idx<180-10){
-		// for (idx = 1; idx < 180; idx++){
+	while(idx<col-20){
+		//print unit of x scale on x-axis
 		if (idx%interval == 0){
 			sprintf(xDigit, "%d", (int)(idx*deltaX));
 			printf("%.3f",idx*deltaX);
 			idx+= (unsigned)strlen(xDigit) + 3;
 		}
+		//spacing between each unit of x-scale
 		else{
 			printf(" ");
 			idx++;
 		}
 	}
 	printf("\n");
-
 	return 0;
 }
 
+//main function of compute the trajectory
 int compute_trajectory(float v_input, float h_input, float theta_input) {
 
 	char str[20] = "hello there";
@@ -165,16 +155,21 @@ int compute_trajectory(float v_input, float h_input, float theta_input) {
 	float deltaY; 
 	int Yindent;
 	float y;	
-	//predefine num is 180
+
+	//display scalling according to global ScreenDisplayScale
+	Xscale = 50*ScreenDisplayScale; //50;
+	Yscale = 150*ScreenDisplayScale; //150;
+	col = 170*ScreenDisplayScale; //180
+	//printf("New Scale factor: %d %d %d\n", Xscale, Yscale, col);
 	
 	v = v_input;
 	h = h_input;
 	theta = theta_input;
-	
 	theta = theta*PI/180;
 
 	printf("\tTRAJECTORY MOTION OF: v = %.5f m/s, h = %.5f m, theta = %.5f radians\n\n",v, h, theta);
 
+	//get max x position and y position
 	dMax = Downfunc(0);
 	yMax = h + pow(v,2)*pow(sin(theta),2)/(2*G_ACC);
 	
@@ -192,7 +187,7 @@ int compute_trajectory(float v_input, float h_input, float theta_input) {
 	deltaX = deltaXY.deltaX;
 	deltaY = deltaXY.deltaY;
 
-	//plotting
+	//plotting graph
 	y = yMax;
 	y = PlotAboveH(y, yMax, deltaX, deltaY, Yindent);
 	y = PlotBelowH(y, deltaX, deltaY, Yindent);
@@ -201,5 +196,4 @@ int compute_trajectory(float v_input, float h_input, float theta_input) {
 	printf("\n\n\nMaximum horizontal distance of the projectile : %f\n\n", dMax);
 
 	return 0;
-    
 }
