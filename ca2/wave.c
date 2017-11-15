f37b11eebc51046730cf4b1c6bed890e2b961a60
Rahul Nambiar <rahul.nambiar@botsync.me>
2017-11-16 02:25:30 +0800

    Included hardware ready check in wave.c

--- ca2/wave.c
+++ ca2/wave.c
@@ -20,16 +20,21 @@ void *generateWave()
 	
 	//find waveform points
 	delta = (2.0*PI)/((float)resolution);
-	printf("%d\n", delta);
-
 	
 	while(1)
 	{	
 		//locking the mutex
 		pthread_mutex_lock(&global_var_mutex);
 
+		//checking if pci bus is initialised before starting output to oscilloscope
+		if hardware_ready == 0:
+			pthread_mutex_unlock(&global_var_mutex);
+			continue;
+
+		//updating variables for wave form 
 		frequency = (global_frequency/255.0)*3000.0;
 	
+		//restricting lower limit of frequency 
 		if (frequency < 0.1)
 			frequency = 0.1;
 
@@ -40,23 +45,24 @@ void *generateWave()
 		//unlocking the mutex
 		pthread_mutex_unlock(&global_var_mutex);
 		
+		//sine wave
 		if (waveType == 0)
 		{
+			//filling data points for sine wave
 			for(i=0;i<resolution;i++)
 			{
 				dummy = (amplitude*sin((float)(i*delta))+amplitude);
 				dummy = ((dummy)/(2.0*MAX_AMP))*0x8000;
-				//printf("Dummy: %f\n", dummy);
 	  			data[i] = (unsigned) dummy;	
 			}	
 		}		
+		//square wave
 		else if(waveType == 1)
 		{
-			printf("Square wave\n");
+			//filling data points for square wave
 			for(i=0;i<resolution;i++)
 			{
 				dummy= amplitude*sin((float)(i*delta));
-				//printf("Dummy: %f\n", dummy);	
 
 				if (dummy > 0.0)
 				{
@@ -70,8 +76,9 @@ void *generateWave()
 			}
 		}
 
+		//determing time period for set frequency 
 		T = 1.0/frequency;
-		//printf("T: %f\n", T);
+		//time step for each data point in wave
 		delta_t = (T)/((float)resolution);
 		t_delta.tv_sec = 0;
 		t_delta.tv_nsec = delta_t*1000000000;
@@ -79,26 +86,40 @@ void *generateWave()
 
 		while(1)
 		{
-			out16(DA_CTLREG,0x0a23);						
-		   	out16(DA_FIFOCLR, 0);							
-		    out16(DA_Data,(short) data[i]);																												
+
+			//setting control register to unipolar 5V
+			out16(DA_CTLREG,0x0a23);		
+			//clearing buffer				
+		   	out16(DA_FIFOCLR, 0);
+			//outputting data to DA port 							
+		    	out16(DA_Data,(short) data[i]);						
+			//suspenduing CPU to adjust frequency of wave
+
 		  	nanospin(&t_delta);
 
+			//incrementing counter
 			++i;
+			//re adjusting counter to 0 when greater than no of points
 			if (i>=resolution)
 			{
 				i=0;
 			}
+			//locking mutex to check if variable update flag has been raised
 			pthread_mutex_lock(&global_var_mutex);
+			//if var has been updated
 			if (var_update == 1)
 			{
+				//restting var update flag
 				var_update = 0;
+				//unlocking mutex before exiting
 				pthread_mutex_unlock(&global_var_mutex);
 				break;
 			}
+			//unlocking mutex
 			pthread_mutex_unlock(&global_var_mutex);		
 		}
 
+		//checking mutex to access state of global kill switch 
 		pthread_mutex_lock(&global_stop_mutex);
 		if (kill_switch==1)
 		{
