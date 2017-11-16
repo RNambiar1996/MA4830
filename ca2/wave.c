42a63173e714f145a0121e805efc5d34d84f9c4e
Rahul Nambiar <rahul.nambiar@botsync.me>
2017-11-16 22:08:34 +0800

    Scaled f and A based on macros. Removed debug printfs

--- ca2/wave.c
+++ ca2/wave.c
@@ -3,7 +3,7 @@
 
 void *generateWave()
 {
-	int resolution = 100;
+	int resolution = 80;
 
 	float delta, T, delta_t, dummy, slope, frequency, amplitude;
 
@@ -12,10 +12,8 @@ void *generateWave()
 
 	struct timespec t_delta;
 	
-	bool waveType, latest_waveType;
-	bool die = 0;
-	bool parameter_changed = 0;
-
+	bool waveType;
+	
 	pthread_sigmask(SIG_SETMASK, &all_sig_mask_set, NULL);
 	if(ThreadCtl(_NTO_TCTL_IO,0)==-1) {
 	  perror("Thread Control");
@@ -27,30 +25,28 @@ void *generateWave()
 	
 	while(1)
 	{	
-		printf("before check mutex wave.c\n");
-		
 		//locking the mutex
 		pthread_mutex_lock(&global_var_mutex);
 
 		//updating variables for wave form 
-		frequency = global_frequency;
-		amplitude = global_amplitude;
+		frequency = (float)global_frequency;
+		amplitude = (float)global_amplitude;
 		waveType = waveform;
 
 		//unlocking the mutex
 		pthread_mutex_unlock(&global_var_mutex);
 		
-		printf("After mutex unlock wave.c\n");		
-		//frequency =
-		
-		if (frequency < 0.1){
-			frequency = 0.1;
-		}
+		if ((int)frequency == 0)
+		    frequency = FREQUENCY_MIN;
+		else if ((int)frequency == 255)
+		    frequency = FREQUENCY_MAX;
+		else
+		    frequency = (frequency/255.0) * FREQUENCY_MAX;
+
 		
 		//sine wave
 		if (waveType == 0)
 		{
-			printf("sine wave\n");
 			//filling data points for sine wave
 			for(i=0;i<resolution;i++)
 			{
@@ -62,7 +58,6 @@ void *generateWave()
 		//square wave
 		else if(waveType == 1)
 		{
-			printf("square wave\n");
 			//filling data points for square wave
 			for(i=0;i<resolution;i++)
 			{
@@ -80,8 +75,6 @@ void *generateWave()
 			}
 		}
 
-		printf("badr: %x\n", badr[0]);
-		printf("iobase: %x\n", iobase[0]);
 		
 		//determing time period for set frequency 
 		T = 1.0/frequency;
@@ -90,38 +83,36 @@ void *generateWave()
 		t_delta.tv_sec = 0;
 		t_delta.tv_nsec = delta_t*1000000000;
 	
-		printf("delta_t: %f\n", delta_t);
-		
 		while(1)
 		{
 
-			//printf("printing to oscilloscope\n");
 			//setting control register to unipolar 5V
 			out16(DA_CTLREG,0x0a23);		
 			//clearing buffer				
 		   	out16(DA_FIFOCLR, 0);
 			//outputting data to DA port 	
-			//printf("short: %d\n", data[i]);
 				
-		    out16(DA_Data,data[i]);						
+		    	out16(DA_Data,data[i]);						
 			//suspenduing CPU to adjust frequency of wave
 
-		  	nanospin(&t_delta);
-		    //delay(1000);
-
+			//to adjust output frequency to oscilloscope		  
+			nanospin(&t_delta);
+		    
 			//incrementing counter
 			++i;
+
 			//re adjusting counter to 0 when greater than no of points
 			if (i>=resolution)
 			{
 				i=0;
 			}
+
 			//locking mutex to check if variable update flag has been raised
 			pthread_mutex_lock(&global_var_mutex);
+
 			//if var has been updated
 			if (var_update == 1)
 			{
-				//printf("var updated\n");
 				//restting var update flag
 				var_update = 0;
 				//unlocking mutex before exiting
